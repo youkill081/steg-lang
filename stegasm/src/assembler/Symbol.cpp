74e73c9c0f7c0b20066c339ffa0aacc290624277
roumite <julien.serin4@gmail.com>
2026-02-26 19:27:48 -0500

    feat(): error handling on multiple symbols with the same name

--- stegasm/src/assembler/Symbol.cpp
+++ stegasm/src/assembler/Symbol.cpp
@@ -4,6 +4,8 @@
 
 #include "./Symbol.h"
 
+#include "assembler_exception.h"
+
 using namespace assembler;
 
 void SymbolSet::insert_symbol(const std::string &name, uint16_t value, SymbolType type)
@@ -15,6 +17,10 @@ SymbolSet SymbolSet::operator+(const SymbolSet &other) const
 {
     SymbolSet result = *this;
     for (const auto &[name, symbol] : other)
+    {
+        if (this->contains(name))
+            throw AssemblerError("Multiple symbol with name " + name + " !");
         result.insert_symbol(name, symbol.value, symbol.type);
+    }
     return result;
 }
